094ea6ae485d60ba90df93731b98dfe6160accca
rossyvee <roseakinyi001@gmail.com>
2022-03-05 15:26:04 +0300

    mapping names with the array-days

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const DAYS = ["sunday" , "monday","tuesday","wednesday","thursday", "friday","saturday"];
+
+const NAMES = {
+    "male": {
+        "sunday": "Kwasi",
+        "monday": "Kwadwo",
+        "tuesday": "Kwabena",
+        "wednesday": "Kwaku",
+        "thursday": "Yaw",
+        "friday": "Kofi",
+        "saturday": "Kwame"
+        },
+    "female": {
+        "sunday": "Akosua",
+        "monday": "Adwoa",
+        "tuesday": "Abenaa",
+        "wednesday": "Akua",
+        "thursday": "Yaa",
+        "friday": "Afua",
+        "saturday": "Ama"
+    }
+}
