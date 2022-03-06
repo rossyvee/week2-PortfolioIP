e30bab561871e1fc9101dd317dd372ee9fa8e7d2
rossyvee <roseakinyi001@gmail.com>
2022-03-06 19:55:16 +0300

    get akan name

--- script.js
+++ script.js
@@ -1,76 +1,102 @@
-
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const full_names = [
-    {
-        "day":"sunday",
-        "gender":{
-            "male":"Kwasi",
-            "female":"Akosua"
-        },
-        "day":"monday",
-        "gender":{
-            "male":"Kwadwo",
-            "female":"Adwoa"
-        },
-        "day":"tuesday",
-        "gender":{
-            "male":"Kwabena",
-            "female":"Abenaa"
-        },
-        "day":"wednesday",
-        "gender":{
-            "male":"Kwaku",
-            "female":"Akua"
-        },
-        "day":"thursday",
-        "gender":{
-            "male":"Yaw",
-            "female":"Yaa"
-        },
-        "day":"friday",
-        "gender":{
-            "male":"Kofi",
-            "female":"Afua"
-        },
-        "day":"saturday",
-        "gender":{
-            "male":"Kwame",
-            "female":"Ama"
-        }
+  {
+    day: "sunday",
+    gender: {
+      male: "Kwasi",
+      female: "Akosua",
+    },
+  },
+  {
+    day: "monday",
+    gender: {
+      male: "Kwadwo",
+      female: "Adwoa",
+    },
+  },
+  {
+    day: "tuesday",
+    gender: {
+      male: "Kwabena",
+      female: "Abenaa",
+    },
+  },
+  {
+    day: "wednesday",
+    gender: {
+      male: "Kwaku",
+      female: "Akua",
+    },
+  },
+  {
+    day: "thursday",
+    gender: {
+      male: "Yaw",
+      female: "Yaa",
+    },
+  },
+  {
+    day: "friday",
+    gender: {
+      male: "Kofi",
+      female: "Afua",
+    },
+  },
+  {
+    day: "saturday",
+    gender: {
+      male: "Kwame",
+      female: "Ama",
+    },
+  },
+];
+
+function getFullNames() {
+  document.getElementById("result-name").innerHTML = "";
+  document.getElementById("name-result").style.display = "none";
+  const string_birthday = document.getElementById("birthday").value;
+
+  const gender = document.getElementById("gender").value;
+
+  let birthday = new Date(string_birthday);
+
+  let day = birthday.getDay();
+
+  let dayOfTheWeek = days[day].toLowerCase();
+
+  console.log({ birthday, dayOfTheWeek });
+
+  let arrLength = full_names.length;
+
+  let akan_name = "";
+
+  for (let index = 0; index < arrLength; index++) {
+    const element = full_names[index];
+
+    if (element.day == dayOfTheWeek) {
+      if (gender == "male") {
+        akan_name += element.gender.male;
+      }
+
+      if (gender == "female") {
+        akan_name += element.gender.female;
+      }
+
+      console.log(element);
     }
-]
+  }
 
-function getDayofTheWeek(century,year,month,day){
-    return ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
+  document.getElementById("result-name").innerHTML = akan_name;
+  document.getElementById("name-result").style.display = "block";
+
+  console.log(`Akan warrior  : ${akan_name}`);
 }
-
-
-function getFullNames(){
-
-    const string_birthday =  document.getElementById("birthday").value;
-
-    let birthday = new Date(string_birthday);
-
-
-    let year = birthday.getFullYear();
-
-    let month =  birthday.getMonth();
-
-    let day = birthday.getDay();
-
-
-    console.log(year)
-    console.log(month)
-    console.log(day)
-
-    console.log({year,month,day})
-
-
-
-    console.log(typeof birthday)
-
-    console.log({birthday})
-
-    console.log(birthday)
-}
-
